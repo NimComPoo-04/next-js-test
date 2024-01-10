4f7e6f6548e24733a451271458d13997f39fd1cb
NimComPoo-04 <bhattacharyyaaanjishnu.04@gmail.com>
2024-01-10 22:25:19 +0530

    created a test to learn sanity api

---
+++ app/layout.js
@@ -0,0 +1,18 @@
+export const metadata = {
+	title: 'Article',
+	description: 'Testing Sanity With Next JS and stuff',
+}
+
+export const viewport = {
+	viewport: "width: device-width, initial-scale: 1.0",
+}
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+		<body>
+		{children}
+		</body>
+		</html>
+	)
+}
